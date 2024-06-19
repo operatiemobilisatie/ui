40551478710314373ff2c2ee98cc9cbede0a12ce
Douwe Pausma <douwepausma@gmail.com>
2024-06-19 16:07:34 +0200

    Test type imports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as Accordion from "@/components/accordion";
-import * as AlertDialog from "@/components/alert-dialog";
-import * as Avatar from "@/components/avatar";
-import * as Badge from "@/components/badge";
-import * as Button from "@/components/button";
-import * as Card from "@/components/card";
-import * as Input from "@/components/input";
-import * as Kicker from "@/components/kicker";
-import * as Label from "@/components/label";
-import * as Progress from "@/components/progress";
-import * as Select from "@/components/select";
-import * as Skeleton from "@/components/skeleton";
-import * as Switch from "@/components/switch";
-import * as Toast from "@/components/toast";
-import * as Toaster from "@/components/toaster";
+import * as Accordion from "./components/accordion";
+import * as AlertDialog from "./components/alert-dialog";
+import * as Avatar from "./components/avatar";
+import * as Badge from "./components/badge";
+import * as Button from "./components/button";
+import * as Card from "./components/card";
+import * as Input from "./components/input";
+import * as Kicker from "./components/kicker";
+import * as Label from "./components/label";
+import * as Progress from "./components/progress";
+import * as Select from "./components/select";
+import * as Skeleton from "./components/skeleton";
+import * as Switch from "./components/switch";
+import * as Toast from "./components/toast";
+import * as Toaster from "./components/toaster";
 import "./globals.css";
 
 export default {Accordion, AlertDialog, Avatar, Badge, Button, Card, Input, Kicker, Label, Progress, Select, Skeleton, Switch, Toast, Toaster}
