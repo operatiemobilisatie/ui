bf2ee45feb5219587e8f0b0c8d545bfd0d8dacdc
Douwe Pausma <douwepausma@gmail.com>
2024-07-02 16:42:22 +0200

    Update InputProps

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -3,7 +3,9 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    displaySize?: 'sm' | 'lg' | 'default'
+  }
 
 const inputVariants = cva(
   "peer flex w-full border border-input bg-background file:mr-4 file:hover:border-primary-400 file:cursor-pointer file:border-border file:border file:border-solid file:bg-transparent file:rounded-2xl file:h-[34px] file:px-4 file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed focus:border-primary disabled:opacity-50 hover:border-gray-300",
@@ -21,7 +23,7 @@ const inputVariants = cva(
   }
 )
 
-const Input = React.forwardRef<HTMLInputElement, InputProps & { displaySize?: "sm" | "lg" | "default" }>(
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, displaySize, type, ...props }, ref) => {
     return (
       <input
