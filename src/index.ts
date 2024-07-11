149eb7e59554241e59ff573b94ed4d18138862c8
Douwe Pausma <douwepausma@gmail.com>
2024-07-11 22:37:54 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Accordion from "./components/accordion";
+import * as Alert from "./components/alert";
 import * as AlertDialog from "./components/alert-dialog";
 import * as Avatar from "./components/avatar";
 import * as Badge from "./components/badge";
@@ -7,7 +8,10 @@ import * as Card from "./components/card";
 import * as Input from "./components/input";
 import * as Kicker from "./components/kicker";
 import * as Label from "./components/label";
+import * as Loading from "./components/loading";
 import * as Progress from "./components/progress";
+import * as RadioGroup from "./components/radio-group";
+import * as RadioButton from "./components/radio-button";
 import * as Select from "./components/select";
 import * as Skeleton from "./components/skeleton";
 import * as Switch from "./components/switch";
@@ -15,7 +19,9 @@ import * as Textarea from "./components/textarea";
 import * as Toast from "./components/toast";
 import * as Toaster from "./components/toaster";
 import * as Tabs from "./components/tabs";
+import * as Toggle from "./components/toggle";
+import * as ToggleGroup from "./components/toggle-group";
 import * as Tooltip from "./components/tooltip";
 import "./globals.css";
 
-export default { Accordion, AlertDialog, Avatar, Badge, Button, Card, Input, Kicker, Label, Progress, Select, Skeleton, Switch, Textarea, Tabs, Toast, Toaster, Tooltip }
+export default { Accordion, Alert, AlertDialog, Avatar, Badge, Button, Card, Input, Kicker, Label, Loading, Progress, RadioGroup, RadioButton, Select, Skeleton, Switch, Textarea, Tabs, Toast, Toaster, ToggleGroup, Tooltip }
