e57554d7d1f5a84c8a43b0ebcd253265be07e5f3
Douwe Pausma <douwepausma@gmail.com>
2024-08-20 10:29:07 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import * as Input from "./components/input";
 import * as Kicker from "./components/kicker";
 import * as Label from "./components/label";
 import * as Loading from "./components/loading";
+import * as Logo from "./components/logo";
 import * as Progress from "./components/progress";
 import * as RadioGroup from "./components/radio-group";
 import * as RadioButton from "./components/radio-button";
@@ -24,4 +25,29 @@ import * as ToggleGroup from "./components/toggle-group";
 import * as Tooltip from "./components/tooltip";
 import "./globals.css";
 
-export default { Accordion, Alert, AlertDialog, Avatar, Badge, Button, Card, Input, Kicker, Label, Loading, Progress, RadioGroup, RadioButton, Select, Skeleton, Switch, Textarea, Tabs, Toast, Toaster, ToggleGroup, Tooltip }
+export default {
+    Accordion,
+    Alert,
+    AlertDialog,
+    Avatar,
+    Badge,
+    Button,
+    Card,
+    Input,
+    Kicker,
+    Label,
+    Loading,
+    Logo,
+    Progress,
+    RadioGroup,
+    RadioButton,
+    Select,
+    Skeleton,
+    Switch,
+    Textarea,
+    Tabs,
+    Toast,
+    Toaster,
+    ToggleGroup,
+    Tooltip
+};
