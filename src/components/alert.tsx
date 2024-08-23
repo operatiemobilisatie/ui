f5f87f13a77d39a1b038d1e98be6e047d3d58ff5
Douwe Pausma <douwepausma@gmail.com>
2024-08-23 18:04:56 +0200

    Add success variant

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -4,13 +4,13 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-xl border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
     variants: {
       variant: {
         default: "bg-background text-foreground",
-        destructive:
-          "border-destructive/50 bg-destructive text-destructive-foreground dark:border-destructive [&>svg]:text-destructive",
+        destructive: "border-destructive/50 bg-destructive text-destructive-foreground dark:border-destructive [&>svg]:text-destructive",
+        success: "border-success/50 bg-success text-success-foreground dark:border-success [&>svg]:text-success",
       },
     },
     defaultVariants: {
