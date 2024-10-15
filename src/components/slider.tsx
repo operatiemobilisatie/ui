ee7066fce9efc16dda4784b166a3b3cd2ce82021
Douwe Pausma <douwepausma@gmail.com>
2024-10-15 15:03:36 +0200

    Create slider.tsx

---
+++ src/components/slider.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+import * as SliderPrimitive from "@radix-ui/react-slider"
+
+import { cn } from "../lib/utils"
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex w-full touch-none select-none items-center pb-6 pt-2",
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb asChild>
+      <div className="flex justify-center h-5 w-5 cursor-grab rounded-full border-[5px] border-primary-200 bg-primary ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+        <div className="text-center translate-y-5 text-sm font-semibold self-center w-min">{props.defaultValue}</div>
+      </div>
+    </SliderPrimitive.Thumb>
+  </SliderPrimitive.Root>
+))
+Slider.displayName = SliderPrimitive.Root.displayName
+
+export { Slider }
