ce0098a93745393f14b3910092dfff1c02090c1e
douwepausma <douwepausma@gmail.com>
2025-03-05 16:55:37 +0100

    useInView instead of react-visibility-sensor

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import * as React from "react"
+import ReactDOM from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
-import { useCountUp } from 'react-countup'
-import VisibilitySensor from "react-visibility-sensor"
+import { useCountUp } from "react-countup"
+import { useInView } from "motion/react"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
@@ -13,6 +14,7 @@ const Progress = React.forwardRef<
 >(({ className, value, ...props }, ref) => {
   const [progress, setProgress] = React.useState(0);
   const counterRef = React.useRef(null);
+  const isInView = useInView(counterRef);
 
   const {start, reset} = useCountUp({
     ref: counterRef,
@@ -22,34 +24,32 @@ const Progress = React.forwardRef<
     suffix: '%'
   });
 
-  const handleVisibilityChange = (isVisible:boolean) => {
-    if (isVisible) {
+  React.useEffect(() => {
+    if (isInView) {
       setProgress(value || 0);
       start();
     } else {
       setProgress(0);
       reset();
     }
-  };
+  }, [isInView]);
 
   return (
-      <VisibilitySensor delayedCall={true} onChange={handleVisibilityChange}>
-        <ProgressPrimitive.Root
-          ref={ref}
-          className={cn(
-            "relative h-6 w-full overflow-hidden rounded-full bg-gray-200",
-            className
-          )}
-          {...props}
+      <ProgressPrimitive.Root
+        ref={ref}
+        className={cn(
+          "relative h-6 w-full overflow-hidden rounded-full bg-gray-200",
+          className
+        )}
+        {...props}
+      >
+        <ProgressPrimitive.Indicator
+          className="h-full w-full flex-1 bg-primary transition-all ease-in-out flex items-center"
+          style={{ transform: `translateX(-${100 - (progress)}%)`, transitionDuration: '1.5s' }}
         >
-          <ProgressPrimitive.Indicator
-            className="h-full w-full flex-1 bg-primary transition-all ease-in-out flex items-center"
-            style={{ transform: `translateX(-${100 - (progress)}%)`, transitionDuration: '1.5s' }}
-          >
-            <div className={`ml-auto text-sm px-2 transition-all ${(progress < 50) ? 'translate-x-full text-black' : 'text-white translate-x-0'}`} ref={counterRef}></div>
-          </ProgressPrimitive.Indicator>
-        </ProgressPrimitive.Root>
-      </VisibilitySensor>
+          <div className={`ml-auto text-sm px-2 transition-all ${(progress < 50) ? 'translate-x-full text-black' : 'text-white translate-x-0'}`} ref={counterRef}></div>
+        </ProgressPrimitive.Indicator>
+      </ProgressPrimitive.Root>
   )
 })
 Progress.displayName = ProgressPrimitive.Root.displayName
