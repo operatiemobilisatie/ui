c73656a6efdcd874b63388a0e337fe1ba71b7274
douwepausma <douwepausma@gmail.com>
2025-08-27 13:37:36 +0200

    fix: set bg color

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -7,7 +7,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-2xl bg-muted", className)}
+      className={cn("animate-pulse rounded-2xl bg-gray-100", className)}
       {...props}
     />
   )
